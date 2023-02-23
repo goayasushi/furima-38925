eafa33fafd58fcc7c27674370d8c0bb54367a555
goayasushi <goayasushi@gmail.com>
2023-02-23 18:50:22 +0900

    userモデルテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,6 @@ class User < ApplicationRecord
     validates :first_name_kana
   end  
   validates :birthday, presence: true
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/.freeze
   validates_format_of :password, with: PASSWORD_REGEX
 end
