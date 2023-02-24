2b33563d3a101eab01342d625d02270673d4d73f
goayasushi <goayasushi@gmail.com>
2023-02-24 17:53:14 +0900

    アソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :charge
+  belongs_to :prefecture
+  belongs_to :preparation
 end
