2dc04fc32457389a7d61946949aa38ed3f71aa47
goayasushi <goayasushi@gmail.com>
2023-03-02 16:14:54 +0900

    購入データ保存機能、エラーメッセージ表示機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,5 +5,21 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order_delivery = OrderDelivery.new(order_params)
+    if @order_delivery.valid?
+      @order_delivery.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
   end
+
+  private
+
+  def order_params
+    params.require(:order_delivery).permit(:postcode, :prefecture_id, :city, :block, :building, :phone ).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+
 end
