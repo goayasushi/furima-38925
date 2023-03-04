73e77ecb40d1b1aa7d693258af5011c90c4999f6
goayasushi <goayasushi@gmail.com>
2023-03-04 15:03:21 +0900

    Rubocop修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -22,7 +22,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_delivery).permit(:postcode, :prefecture_id, :city, :block, :building, :phone ).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_delivery).permit(:postcode, :prefecture_id, :city, :block, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
